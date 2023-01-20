e035f301e392be0551ad0bfcdf7194c6fcd285af
fraperrone <fperronerey@gmail.com>
2023-01-20 03:29:24 -0300

    Estructura OK - SPINNER con Bootstrap

--- Proyecto_Final/js/API_fetch.js
+++ Proyecto_Final/js/API_fetch.js
@@ -1,3 +1,14 @@
+var $cargando = document.getElementById("cargando").style;
+var $datos = document.getElementById("datos").style;
+var $picture = document.getElementById("picture").style;
+$cargando.display = "block";
+$datos.display = "none";
+$picture.display = "none";
+
+console.log($cargando.display);
+/**
+ * Fetch
+ */
 fetch("https://randomuser.me/api/")
   .then((data) => data.json())
   .then((post) => {
@@ -10,4 +21,10 @@ fetch("https://randomuser.me/api/")
       "Apellido: " + reg.name.last;
     document.getElementById("titulo").innerHTML = "Titulo: " + reg.name.title;
     document.getElementById("picture").src = reg.picture.large;
+  })
+  .catch((error) => console.log(error))
+  .finally(() => {
+    $cargando.display = "none";
+    $datos.display = "block";
+    $picture.display = "block";
   });
--- Proyecto_Final/readme.md
+++ Proyecto_Final/readme.md
@@ -3,7 +3,10 @@ Proyecto Front-End
 El cual toma de una API REST la información de datos personales e imagen,
 las habilidades y experiencias no brinda información la API, por lo que se pone un "Lorem ipsum".
 
+recordar para que la pagina funcione correctamente debe tener acceso internet y a la API: "https://randomuser.me/api/"
+
 Ante dudas o sugerencias, mi contacto: Fperronerey@gmail.com
 
 **************************************************************************
 En la carpeta diseño encontramos el boceto previo.
+
